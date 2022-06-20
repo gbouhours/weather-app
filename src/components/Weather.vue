bbfd8cd282ed572e25648cfafcdffdc8dbde5bb9
gbouhours NA <greguix@yahoo.com>
2022-06-21 00:29:11 +0200

    feat: add icon weather

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,33 +1,43 @@
 <template>
-  <div class="container mx-auto max-w-screen-xl p-4">
-    <h1 class="text-3xl text-white text-center mb-6">App météo avec Vue.js</h1>
-      <div class="w-full md:w-3/5 mx-auto border-2 border-cyan-800 rounded-lg bg-white p-4 mb-12" v-if="weather_position">
-        <p class="text-center text-base font-sans text-stone-900 mb-3">La météo à : <span class="text-xl">{{city}}</span></p>
-        <p class="text-center text-base font-sans text-stone-900 mb-3">Température : {{weather_position.main.temp.toFixed()}}°C</p>
-        <p class="text-center text-base font-sans text-stone-900 mb-3">Température ressenti : {{weather_position.main.feels_like.toFixed()}}°C</p>
-        <p class="text-center text-base font-sans text-stone-900 mb-3">Temps : {{weather_position.weather[0].description}}</p>
-        <p class="text-center text-base font-sans text-stone-900 mb-3">Taux humidité : {{weather_position.main.humidity}}%</p>
-        <p class="text-center text-base font-sans text-stone-900 mb-3">Vent : {{(weather_position.wind.speed*3600/1000).toFixed()}}km/h</p>
-      </div>
-      <div class="mb-12 w-full md:w-3/5 mx-auto">
-        <label for="position" class="block mb-2 text-lg font-medium text-white">Entrer le nom d'une ville</label>
-        <input 
-          type="text" 
-          id="position" 
-          class="bg-secondary-50 border-2 border-cyan-800 text-blue-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          v-model="request"
-          @keypress.enter="getWeather"
-          >
-      </div>
-      <div class="w-full md:w-3/5  mx-auto border-2 border-cyan-800 rounded-lg bg-white p-4 mb-12" v-if="weather">
-        <p class="text-center text-base font-sans text-stone-900 mb-3">La météo à : <span class="text-xl">{{weather.name}}</span></p>
-        <p class="text-center text-base font-sans text-stone-900 mb-3">Température : {{weather.main.temp.toFixed()}}°C</p>
-        <p class="text-center text-base font-sans text-stone-900 mb-3">Température ressenti : {{weather.main.feels_like.toFixed()}}°C</p>
-        <p class="text-center text-base font-sans text-stone-900 mb-3">Temps : {{weather.weather[0].description}}</p>
-        <p class="text-center text-base font-sans text-stone-900 mb-3">Taux humidité : {{weather.main.humidity}}%</p>
-        <p class="text-center text-base font-sans text-stone-900 mb-3">Vent : {{(weather.wind.speed*3600/1000).toFixed()}}km/h</p>
+  <div class="container max-w-screen-xl p-4 mx-auto">
+    <h1 class="mb-6 text-3xl text-center text-white">App météo avec Vue.js</h1>
+    <div class="w-full p-4 mx-auto mb-12 bg-white border-2 rounded-lg md:w-3/5 border-cyan-800" v-if="weather_position">
+      <p class="mb-3 font-sans text-base text-center text-stone-900">La météo à : <span class="text-xl">{{city}}</span>
+      </p>
+      <p class="mb-3 font-sans text-base text-center text-stone-900">Température :
+        {{weather_position.main.temp.toFixed()}}°C</p>
+      <p class="mb-3 font-sans text-base text-center text-stone-900">Température ressenti :
+        {{weather_position.main.feels_like.toFixed()}}°C</p>
+      <p class="mb-3 font-sans text-base text-center text-stone-900">Temps : {{weather_position.weather[0].description}}
+      </p>
+      <p class="mb-3 font-sans text-base text-center text-stone-900">Taux humidité : {{weather_position.main.humidity}}%
+      </p>
+      <p class="mb-3 font-sans text-base text-center text-stone-900">Vent :
+        {{(weather_position.wind.speed*3600/1000).toFixed()}}km/h</p>
+      <img :src="`http://openweathermap.org/img/wn/${weather_position.weather[0].icon}@2x.png`"
+        :alt="`${weather_position.weather[0].description}`" class="mx-auto">
+    </div>
+    <div class="w-full mx-auto mb-12 md:w-3/5">
+      <label for="position" class="block mb-2 text-lg font-medium text-white">Entrer le nom d'une ville</label>
+      <input type="text" id="position"
+        class="bg-secondary-50 border-2 border-cyan-800 text-blue-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        v-model="request" @keypress.enter="getWeather">
+    </div>
+    <div class="w-full p-4 mx-auto mb-12 bg-white border-2 rounded-lg md:w-3/5 border-cyan-800" v-if="weather">
+      <p class="mb-3 font-sans text-base text-center text-stone-900">La météo à : <span
+          class="text-xl">{{weather.name}}</span></p>
+      <p class="mb-3 font-sans text-base text-center text-stone-900">Température : {{weather.main.temp.toFixed()}}°C
+      </p>
+      <p class="mb-3 font-sans text-base text-center text-stone-900">Température ressenti :
+        {{weather.main.feels_like.toFixed()}}°C</p>
+      <p class="mb-3 font-sans text-base text-center text-stone-900">Temps : {{weather.weather[0].description}}</p>
+      <p class="mb-3 font-sans text-base text-center text-stone-900">Taux humidité : {{weather.main.humidity}}%</p>
+      <p class="mb-3 font-sans text-base text-center text-stone-900">Vent :
+        {{(weather.wind.speed*3600/1000).toFixed()}}km/h</p>
+      <img :src="`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`"
+        :alt="`${weather.weather[0].description}`" class="mx-auto">
 
-      </div>
+    </div>
   </div>
 </template>
 <script>
